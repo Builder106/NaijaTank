4ac79627c37cd080af93625799dd2b6869aa06ea
Builder106 <vaughanolayinka@gmail.com>
2025-05-25 18:57:47 +0100

    Removal of Supabase dir

--- supabase/functions/_shared/cors.ts
+++
@@ -1,5 +0,0 @@
-export const corsHeaders = {
-  'Access-Control-Allow-Origin': '*', // Or your specific frontend domain in production
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
-  'Access-Control-Allow-Methods': 'GET, POST, OPTIONS', // Added OPTIONS and other methods if needed
-}; 
