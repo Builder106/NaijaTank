be839d262cdf0b3de5e6da561ca0bfa2e6c99d02
Builder106 <vaughanolayinka@gmail.com>
2025-06-12 13:50:17 +0100

    Deleted environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-   production: true,
-   apiUrl: import.meta.env.NG_APP_SUPABASE_URL + '/rest/v1',
-   googleMapsApiKey: import.meta.env.NG_APP_GOOGLE_MAPS_API_KEY,
-   supabaseUrl: import.meta.env.NG_APP_SUPABASE_URL,
-   supabaseKey: import.meta.env.NG_APP_SUPABASE_KEY
- };
